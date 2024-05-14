f1c6cd36f3d30e08c992c9a9473213ce5e43a0f8
Normando Zubia <nzubia@blacklabs.mx>
2024-05-14 17:16:27 -0600

    Se realiza setup inicial del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" alt="Pokémon" class="logo" style="width: 600px; height: auto;">
+    <h2>¿Creés ser el mejor maestro pokemon del mundo?</h2>
+    <h3>Memoriza la mayor cantidad de Pokemons y demuestralo!!</h3>
+    <h1>Equipo elegido para esta ronda:</h1>
+    <div v-if="equipoPokemon.length===0">
+      <h4>Cargando equipo Pokemon...</h4>
+    </div>
+    <div class="button-container" v-else>
+      <ImageButton
+          v-for="(imageSrc, index) in equipoPokemon"
+          :key="index"
+          :imageSrc="imageSrc"
+      />
+    </div>
+    <button class="start-button"
+            v-if="equipoPokemon.length!==0"
+    ></button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import ImageButton from './components/ImageButton.vue';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      equipoPokemon: []
+    }
+  },
   components: {
-    HelloWorld
+    ImageButton
+  },
+  methods: {
+    ObtenerEquipoInicial (){
+
+    }
+  },
+  mounted: {
+
+  },
+  created: {
+
   }
 }
 </script>
@@ -24,5 +56,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.button-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+}
+
+.start-button {
+  width: 150px; /* Ajusta el tamaño según sea necesario */
+  height: 50px; /* Ajusta el tamaño según sea necesario */
+  background-image: url('https://static.vecteezy.com/system/resources/thumbnails/017/522/394/small/button-for-8-bit-games-start-beginning-vector.jpg');
+  background-size: cover;
+  background-position: center;
+  border: none;
+  cursor: pointer;
+  transition: transform 0.2s;
+}
+
+.start-button:hover {
+  transform: scale(1.1);
 }
 </style>
---
+++ src/components/ImageButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="image-button">
+    <img :src="imageSrc" :alt="altText" />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    imageSrc: {
+      type: String,
+      required: true,
+    },
+    altText: {
+      type: String,
+      default: 'Button Image',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.image-button {
+  cursor: pointer;
+  padding: 10px;
+}
+
+.image-button img {
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 10px;
+  transition: transform 0.2s;
+}
+
+.image-button:hover img {
+  transform: scale(1.1);
+}
+</style>
