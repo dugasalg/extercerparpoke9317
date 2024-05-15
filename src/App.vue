c754df04e8d7928056f845392c8c17c9b9662aad
Normando Zubia <nzubia@blacklabs.mx>
2024-05-14 18:14:41 -0600

    Se agrega secuencia inicial de juego

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,62 @@
 <template>
   <div id="app">
     <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" alt="Pokémon" class="logo" style="width: 600px; height: auto;">
-    <h2>¿Creés ser el mejor maestro pokemon del mundo?</h2>
-    <h3>Memoriza la mayor cantidad de Pokemons y demuestralo!!</h3>
+    <h2 v-if="!juegoIniciado">¿Eres el mejor maestro pokemon del mundo?</h2>
+    <h3 v-if="!juegoIniciado">Memoriza la mayor cantidad de Pokemons y demuestralo!!</h3>
     <h1>Equipo elegido para esta ronda:</h1>
     <div v-if="equipoPokemon.length===0">
       <h4>Cargando equipo Pokemon...</h4>
     </div>
     <div class="button-container" v-else>
       <ImageButton
-          v-for="(imageSrc, index) in equipoPokemon"
+          v-for="(objectInfo, index) in equipoPokemon"
           :key="index"
-          :imageSrc="imageSrc"
+          :imageSrc="objectInfo.imagenUrl"
       />
     </div>
+    <br>
+    <br>
     <button class="start-button"
-            v-if="equipoPokemon.length!==0"
+            v-if="equipoPokemon.length!==0 && !juegoIniciado"
+            v-on:click="iniciarJuego"
     ></button>
+    <div v-if="juegoIniciado">
+
+    </div>
   </div>
 </template>
 
 <script>
 
 import ImageButton from './components/ImageButton.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   data(){
     return {
-      equipoPokemon: []
+      equipoPokemon: [],
+      juegoIniciado: false
     }
   },
   components: {
     ImageButton
   },
   methods: {
-    ObtenerEquipoInicial (){
+    async obtenerEquipoInicial (){
+      const response = await axios.get('https://pear-pig-kit.cyclic.app/equipoInicial');
 
+      this.equipoPokemon = response.data.equipoInicial;
+    },
+    iniciarJuego(){
+      this.juegoIniciado = true;
     }
   },
-  mounted: {
+  mounted() {
 
   },
-  created: {
-
+  created() {
+    this.obtenerEquipoInicial();
   }
 }
 </script>
